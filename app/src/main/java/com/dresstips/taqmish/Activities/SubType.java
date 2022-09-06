f88234fa27416fcb2b67050c0adf17c291ef362d
Namrouti <62179615+Namrouti@users.noreply.github.com>
2022-09-06 11:33:27 +0300

    من المقر

--- app/src/main/java/com/dresstips/taqmish/Activities/SubType.java
+++ app/src/main/java/com/dresstips/taqmish/Activities/SubType.java
@@ -3,14 +3,22 @@ package com.dresstips.taqmish.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dresstips.taqmish.R;
 
 public class SubType extends AppCompatActivity {
-
+    TextView mainClassName;
+    ImageView classImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_type);
+        mainClassName = findViewById(R.id.mainClassName);
+        classImage = findViewById(R.id.classIcon);
+        mainClassName.setText( getIntent().getExtras().getString("englishName"));
+
+
     }
 }
